0df773fe395f72bf3e021ace8f13564e057702b3
Onur Com <comkael@gmail.com>
2020-05-14 21:31:07 +0200

    gitignore

---
+++ CoffeeGo/CGTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CGTableViewCell.swift
+//  CoffeeGo
+//
+//  Created by Onur Com on 8.05.2020.
+//  Copyright © 2020 Onur Com. All rights reserved.
+//
+
+import UIKit
+
+class CGTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ CoffeeGo/StadiumsModel.swift
@@ -0,0 +1,9 @@
+//
+//  StadiumsModel.swift
+//  CoffeeGo
+//
+//  Created by Onur Com on 5.05.2020.
+//  Copyright © 2020 Onur Com. All rights reserved.
+//
+
+import Foundation
